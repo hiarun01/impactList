255bfd635800982a62a43bf3baee02bdb015e8eb
hiarun01 <hiarun02@gmail.com>
2025-06-23 20:11:50 +0530

    some class chnages

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-white border-t border-gray-200 py-6  text-center text-gray-600 text-sm">
+    <footer className="bg-white  py-6  text-center text-gray-600 text-sm">
       <div className="container mx-auto px-4 flex flex-col md:flex-row justify-center items-center gap-2">
         <div className="flex justify-center items-center">
           <a
