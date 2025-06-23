81bc6791fdd2b5d968f07d70856524a47480ed6d
hiarun01 <hiarun02@gmail.com>
2025-06-23 15:50:49 +0530

    add MarqueeComponent

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import HeroSection from "@/HeroSection";
+import {MarqueeComponent} from "@/MarqueeComponent";
+
 import React from "react";
 
 const Home = () => {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <MarqueeComponent />
+    </>
+  );
 };
 
 export default Home;
